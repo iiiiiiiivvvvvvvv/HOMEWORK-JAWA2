cf42c17cc4f6980de002330c4f8e291b9cf65aad
Igor <oligor2@yandex.ru>
2023-02-20 10:17:45 +0500

    01

--- hometask1.java
+++ hometask1.java
@@ -1,43 +1,60 @@
+// Реализовать алгоритм сортировки слиянием.
 
-
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-// Дана строка sql-запроса "select * from students where ".
-//  Сформируйте часть WHERE этого запроса, используя StringBuilder.
-//  Данные для фильтрации приведены ниже в виде json строки.
-// Если значение null, то параметр не должен попадать в запрос.
-// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia",
-//  "city":"Moscow", "age":"null"}
+import java.util.Arrays;
+import java.util.Random;
 
 public class hometask1 {
-    public static void main(String[] args) {
-        Map<String, String> map = new LinkedHashMap<>();
-        map.put("name","Ivanov");
-        map.put("country","Russia");
-        map.put("city","Moscow");
-        map.put("age",null);
-        map.put("name1","Ivanov");
-        map.put("name2","Ivanov");
 
-        System.out.println(getQuery(map));
+    public static void main(String[] args) {
+        int[] array1 = randomArr();
+        int[] result = mergesort(array1);
+        System.out.println(Arrays.toString(result));
     }
 
-    public static String getQuery(Map<String, String> params) {
-        StringBuilder result = new StringBuilder();
-        if (params == null || params.isEmpty())
-            return result.toString();
+    public static int[] randomArr() {
+        Random rand = new Random();
+        int arr[] = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+        return arr;
+    }
 
-        for (Map.Entry<String, String> pair : params.entrySet()) {
-            if (pair.getKey() == null || pair.getValue() == null)
-                continue;
+    public static int[] mergesort(int[] array1) {
+        int[] buffer1 = Arrays.copyOf(array1, array1.length);
+        int[] buffer2 = new int[array1.length];
+        int[] result = mergesortInner(buffer1, buffer2, 0, array1.length);
+        return result;
+    }
 
-            result.append(pair.getKey()).append(" = '").append(pair.getValue()).append("' and ");
+    public static int[] mergesortInner(int[] buffer1, int[] buffer2,
+            int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return buffer1;
         }
 
-        if (result.length() > 5)
-            result.delete(result.length() - 5, result.length());
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sort1 = mergesortInner(buffer1, buffer2, startIndex, middle);
+        int[] sort2 = mergesortInner(buffer1, buffer2, middle, endIndex);
 
-        return result.toString();
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sort1 == buffer1 ? buffer2 : buffer1;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sort1[index1] < sort2[index2]
+                    ? sort1[index1++]
+                    : sort2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sort1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sort2[index2++];
+        }
+        return result;
     }
+
 }
